6e4c8bb9b8edb9add9aa8c1a8a396bdbc1345038
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-12-18 22:54:05 -0600

    installed npmp packages and began the require code for the server.js file.

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require ("path");
+const express = require ("express");
+
+//ROUTES
+
+//WAHT THE USER PUTS IN THEIR BROWSER -> WHAT WILL IT GO TO, WHAT DATA WILL THEY SEE.
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
+  public/survey.html
+www.google.com/
+
+app.get('/api/survey', function asdfasdfas)
+
