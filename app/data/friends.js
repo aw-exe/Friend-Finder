613de7fd8e2ee5cd60ca15362a8befe2280955f0
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-12-21 01:43:51 -0600

    installed npm package body parser, created the boiler plate for my database of friends (need to build out each friend), and made adjustments to my server.js file. In the server.js file I required the packages I installed, made sure port 3000 was still working from my previous commit, called bodyparser to set up for the code in my other files.

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+const friends = [
+    {
+        name: "Tom Hardy",
+        photo: "picturefilehere"
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+
+    {
+        name: "Tom Hardy",
+        photo: "picturefilehere"
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+
+    {
+        name: "Tom Hardy",
+        photo: "picturefilehere"
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+
+    {
+        name: "Tom Hardy",
+        photo: "picturefilehere"
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+
+    {
+        name: "Tom Hardy",
+        photo: "picturefilehere"
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    }
+];
+
+module.exports = friends;
