ad60afed5e232e71569c1361e1d5cb06d21d07dc
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-12-21 10:11:16 -0600

    began working on the api routes javascript....I have no idea what I'm doing and it still doesn't work

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,11 +5,11 @@ const path = require('path');
 module.exports =  function (app){
 
 // GET ROUTE FOR APP 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 // POST ROUTE FOR SURVEY ANSWERS
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
 }
