02625ba89a3bd65ca14b0b4bdf0adb6de23531ee
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2019-12-21 01:11:18 -0600

    began trying to set up my routes in my htmlRoutes.js and server.js files, also made sure I could connect to port 3000.

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+// Setting HTML routes for user  
+
+const path = require('path');
+
+module.exports =  function (app){
+
+// GET ROUTE FOR APP 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+  });
+// POST ROUTE FOR SURVEY ANSWERS
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+  });
+}
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
-const path = require ("path");
-const express = require ("express");
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
 
 //ROUTES
 
 //WAHT THE USER PUTS IN THEIR BROWSER -> WHAT WILL IT GO TO, WHAT DATA WILL THEY SEE.
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
-  });
-  public/survey.html
-www.google.com/
+app.get("/survey", function (req, res) {
+  res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
 
 
+require("./app/routing/htmlRoutes")(app);
+// require("/apiRoutes")(app);
+
+app.listen(PORT, () =>
+  console.log(`The server is listening on port ${PORT}!`));
