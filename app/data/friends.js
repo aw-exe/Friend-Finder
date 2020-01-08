2f3e53b7759a61686a5e351aaaf39601cb54502f
Ashton Wetsel <ashton.wetsel.23@gmail.com>
2020-01-07 18:51:04 -0600

    Changed values for each friend in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,31 +2,31 @@ const friends = [
     {
         name: "Sally",
         photo: "picturefilehere",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [1, 3, 6, 5, 7, 3, 5, 3, 2, 1]
     },
 
     {
         name: "Tom",
         photo: "picturefilehere",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [4, 3, 4, 2, 8, 4, 2, 6, 9, 4]
     },
 
     {
         name: "David",
         photo: "picturefilehere",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [7, 5, 7, 4, 9, 1, 3, 4, 6, 7]
     },
 
     {
         name: "Carol",
         photo: "picturefilehere",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [6, 7, 9, 5, 4, 7, 3, 6, 5, 3]
     },
 
     {
         name: "Bobby",
         photo: "picturefilehere",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [3, 9, 8, 3, 2, 1, 9, 8, 4, 3]
     }
 ];
 
